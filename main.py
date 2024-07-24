96fcb9bc2cf91083d33447cbd2e79c587a824cbc
Raka Luthfi <raka.luthfi@bankmega.com>
2024-07-24 09:54:01 +0700

    fire

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from time import time
 from fastapi import FastAPI, __version__
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
+from google.cloud import firestore
+import os
 
 app = FastAPI()
+encoded_credentials = os.getenv("FIRESTORE_CREDENTIALS")
+if not encoded_credentials:
+    raise ValueError("FIRESTORE_CREDENTIALS environment variable is not set")
+# Initialize Firestore client
+db = firestore.Client()
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 html = f"""
@@ -29,6 +39,20 @@ html = f"""
 @app.get("/")
 async def root():
     return HTMLResponse(html)
+
+@app.get("/qna/{category}/{sub_category}")
+async def read_item(category: str, sub_category: str):
+    try:
+        # Get document from Firestore
+        doc_ref = db.collection(category).document(sub_category)
+        print(doc_ref)
+        doc = doc_ref.get()
+        if doc.exists:
+            return doc.to_dict()
+        else:
+            raise HTTPException(status_code=404, detail="Item not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get('/ping')
 async def hello():
